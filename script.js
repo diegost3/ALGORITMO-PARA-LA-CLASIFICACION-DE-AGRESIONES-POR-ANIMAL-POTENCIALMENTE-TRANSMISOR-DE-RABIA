ea1f9097321f1cf098e3fe0da9f0cc10bdf7f707
diegost3 <57415631+diegost3@users.noreply.github.com>
2024-11-27 23:07:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function nextQuestion(key) {
 
             // Después de la alerta, regresar al inicio
             nextQuestion("start");
-        }, 1000); // Mostrar la alerta después de un segundo
+        }, 3000); // Mostrar la alerta después de tres segundos
     } else if (key === "exposicion_leve") {
         appContainer.classList.add("exposicion-leve");
     } else if (key === "exposicion_grave") {
